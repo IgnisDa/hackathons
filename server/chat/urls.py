7d8f2ca79eda21bfd4a607b91bcb39a1697f395f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 23:12:54 +0550

    feat(urls.py): add url for fetching all interests

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('<str:room_name>/', views.room, name='room'),
+    path("interests/", views.get_all_interests_view, name="interests"),
+    path("<str:room_name>/", views.room, name="room"),
 ]
