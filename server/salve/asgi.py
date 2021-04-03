f24cacde3f603f2709d046eca4a240f09ab04a86
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 21:26:13 +0000

    change imports

--- server/salve/asgi.py
+++ server/salve/asgi.py
@@ -1,13 +1,14 @@
 import os
 
-import chat.routing
 import django
+django.setup()
+
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salve.settings")
-django.setup()
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
