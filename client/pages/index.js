7a7737d0fb66bfb91bf6c09336160d739e908085
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 19:04:02 +0550

    style(index.js): linting

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,19 @@
 import React from "react";
-import Head from "next/head";
-import Link from "next/link";
-import GoogleFontsComponent from "../src/components/GoogleFonts";
-import Banner from '../src/components/index/Banner.js'
+import Banner from "../src/components/index/Banner.js";
 import Layout from "../src/components/Layout";
 import Footer from "../src/components/Footer";
-
 
 export default function Home() {
   return (
     <React.Fragment>
       <Layout noFooter>
-      <div className="flex flex-col justify-between h-screen bg-not-quite-black">
-        <main className="flex flex-col items-center justify-center flex-grow space-y-5">
-          <Banner />
-        </main>
-      <Footer />
-      </div>
-    </Layout>
+        <div className="flex flex-col justify-between h-screen bg-not-quite-black">
+          <main className="flex flex-col items-center justify-center flex-grow space-y-5">
+            <Banner />
+          </main>
+          <Footer />
+        </div>
+      </Layout>
     </React.Fragment>
   );
 }
