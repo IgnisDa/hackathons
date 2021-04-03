113629c44babda6d81114fbc790c3419bbfab6a6
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 12:57:16 +0550

    feat(models.py): add method to get last 30 messages

--- server/chat/models.py
+++ server/chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from . import managers
+
+
+class Message(models.Model):
+    author = models.CharField(max_length=200, help_text=_("The author of this message"))
+    content = models.TextField(help_text=_("The contents of the message"))
+    timestamp = models.DateTimeField(
+        auto_now_add=True, help_text=_("The time at which the message was sent at")
+    )
+
+    objects = managers.MessageManager()
+
+    def __str__(self):
+        return f"Sent by {self.author} at {self.timestamp}"
