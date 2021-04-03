593addab71eb3a5c086f9e98f8ab7ad2291509c1
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 12:01:36 +0550

    feat(GoogleFonts.js, Banner.js): add components to abstract landing page

---
+++ client/src/components/GoogleFonts.js
@@ -0,0 +1,8 @@
+import { GoogleFonts } from "next-google-fonts";
+import React from "react";
+
+export default function Layout() {
+  return (
+    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@500&display=swap" />
+  );
+}
---
+++ client/src/components/index/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Banner() {
+  return (
+    <React.Fragment>
+      <div className="uppercase transition-colors duration-300 select-none text-blurple text-7xl md:text-8xl xl:text-9xl font-kiwi hover:text-indigo-600">
+        Salve
+      </div>
+      <Link href={{ pathname: "survey", query: { question: 1 } }}>
+        <div className="px-4 py-2 font-bold tracking-widest uppercase transition-colors duration-300 border-2 rounded-sm cursor-pointer sm:text-lg md:text-xl text-spotify-green hover:text-black hover:bg-spotify-green border-spotify-green hover:border-black">
+          Take the initiation test
+        </div>
+      </Link>
+    </React.Fragment>
+  );
+}
