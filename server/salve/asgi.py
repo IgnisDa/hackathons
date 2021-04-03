1552339444f40fd692d05b3641d1d34c0b518726
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-04 02:12:41 +0550

    feat(Procfile, asgi.py, settings.py): add heroku settings

--- server/salve/asgi.py
+++ server/salve/asgi.py
@@ -1,18 +1,16 @@
-# mysite/asgi.py
 import os
 
+import chat.routing
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salve.settings")
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+django.setup()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+    }
+)
