b5f6011fc9ea292c2629afb3ecd1ab4676f52240
Muskan Vaswan <muskanvaswan@gmail.com>
2021-04-03 22:49:58 +0530

    feat(tailwind.config.js): configuring theme in tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blurple: "#7289DA",
-        "spotify-green": "#1DB954",
+        blurple: "#E3AFBC",
+        "spotify-green": "rgba(221, 99, 232, 1)",
         "dark-but-not-black": "#2C2F33",
-        "not-quite-black": "#23272A",
-        "almost-black": "#202225",
-        "light-black": "#36393F",
+        "not-quite-black": "rgba(154, 23, 80,1)",
+        "paper": "rgba(154, 23, 80,0.3)",
+        "paperLight": "rgba(227, 175, 188, 0.3)",
+        "almost-black": "#5D001E",
+        "light-black": "#E3E2DF",
       },
     },
     fontFamily: {
