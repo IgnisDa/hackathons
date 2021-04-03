a6d119c993110c44b5791c74309ecdbf12f34519
Muskan Vaswan <muskanvaswan@gmail.com>
2021-04-03 16:24:24 +0530

    feat: figuring out channel name

--- client/src/components/chat/App.js
+++ client/src/components/chat/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { useRouter } from 'next/router'
 
 import Chat from "./Chat";
 import WebSocketInstance from "../../websocket.js";
 
 export default class App extends React.Component {
   componentDidMount() {
+
     WebSocketInstance.connect();
+    console.log(this.props)
   }
 
   render() {
-    return <Chat />;
+    return <Chat roomName={this.props.roomName}/>;
   }
 }
