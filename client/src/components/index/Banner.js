a2dcc4ff892e7ef318f0f47a88ff442e83138e92
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 19:39:45 +0550

    feat(Banner.js): made banner text responsive

--- client/src/components/index/Banner.js
+++ client/src/components/index/Banner.js
@@ -1,19 +1,14 @@
 import React from "react";
 import Link from "@material-ui/core/Link";
-import Typography from "@material-ui/core/Typography";
 
 export default function Banner() {
   return (
     <React.Fragment>
-      <Typography
-        variant="h1"
-        component="div"
-        className="uppercase transition-colors duration-300 select-none text-blurple text-7xl md:text-8xl xl:text-9xl hover:text-indigo-600"
-      >
+      <div className="text-6xl tracking-widest uppercase transition-colors duration-300 select-none text-blurple sm:text-7xl md:text-8xl xl:text-9xl hover:text-indigo-600">
         Salve
-      </Typography>
+      </div>
       <Link href="/connect" underline="none">
-        <div className="px-4 py-2 font-bold tracking-widest uppercase transition-colors duration-300 border-2 rounded-sm cursor-pointer sm:text-lg md:text-xl text-spotify-green hover:text-black hover:bg-spotify-green border-spotify-green hover:border-black">
+        <div className="px-2 py-2 text-sm font-bold tracking-widest uppercase transition-colors duration-300 border-2 rounded-sm cursor-pointer md:px-4 sm:text-lg md:text-xl text-spotify-green hover:text-black hover:bg-spotify-green border-spotify-green hover:border-black">
           Take the initiation test
         </div>
       </Link>
