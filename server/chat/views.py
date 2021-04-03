d5fe9f6440c3cf25d79c3502a5425cdd017d1afa
Muskan Vaswan <muskanvaswan@gmail.com>
2021-04-03 17:41:39 +0530

    feat: draft functionality to find best suited interest

--- server/chat/views.py
+++ server/chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Interest, Channel
+from django.http import JsonResponse
 
 def index(request):
     return render(request, "chat/index.html")
@@ -8,3 +9,14 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+def most_frequent(l):
+    return max(set(locals), key = list.count)
+
+def induct(request):
+    data = request.data
+    interests = data['interests']
+    possible_channels = []
+    interest_channels = [[possible_channels.append(j) for j in set(Interest.objects.get(pk=i).channels)] for i in interests]
+    channel = most_frequent(possible_channels)
+    return JsonResponse({channel: channel})
