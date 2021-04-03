24410d3987b5d7d8bd3fc5fd5dac08a432675c77
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 20:11:58 +0550

    refactor(App.js): remove useless imports

--- client/src/components/chat/App.js
+++ client/src/components/chat/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 import Chat from "./Chat";
 import WebSocketInstance from "../../websocket.js";
 
 export default class App extends React.Component {
   componentDidMount() {
-
     WebSocketInstance.connect();
-    console.log(this.props)
   }
 
   render() {
-    return <Chat roomName={this.props.roomName}/>;
+    return <Chat roomName={this.props.roomName} />;
   }
 }
