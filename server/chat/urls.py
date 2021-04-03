f25f90943ed364e14709ba03cbbb437aa1d40fbb
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 23:39:36 +0550

    feat(urls.py): add induction view

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("induct/", views.induct_view, name="induct"),
     path("interests/", views.get_all_interests_view, name="interests"),
     path("<str:room_name>/", views.room, name="room"),
 ]
