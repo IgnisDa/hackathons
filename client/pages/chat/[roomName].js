90ef685175824226f9574731f9c3296af58ae103
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-03 19:19:00 +0550

    style([roomName].js): linting

--- client/pages/chat/[roomName].js
+++ client/pages/chat/[roomName].js
@@ -1,18 +1,17 @@
 import React from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 import App from "../../src/components/chat/App.js";
 import Layout from "../../src/components/Layout";
 
 export default function ChatRoom() {
-  const router = useRouter()
-  const { roomName } = router.query
+  const router = useRouter();
+  const { roomName } = router.query;
 
   return (
     <React.Fragment>
-      <Layout short brightFooter>
-
-        <App roomName={roomName}/>
+      <Layout noFooter>
+        <App roomName={roomName} />
       </Layout>
     </React.Fragment>
   );
