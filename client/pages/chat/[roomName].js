66dfeb57d20818de0ef536ee3c7a88b1e78f3a71
Muskan Vaswan <muskanvaswan@gmail.com>
2021-04-03 16:23:03 +0530

    feat: configuring request to correct channel from localstorage

---
+++ client/pages/chat/[roomName].js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useRouter } from 'next/router'
+
+import App from "../../src/components/chat/App.js";
+import Layout from "../../src/components/Layout";
+
+export default function ChatRoom() {
+  const router = useRouter()
+  const { roomName } = router.query
+
+  return (
+    <React.Fragment>
+      <Layout short brightFooter>
+
+        <App roomName={roomName}/>
+      </Layout>
+    </React.Fragment>
+  );
+}
