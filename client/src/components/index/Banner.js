bb05ebd7c0799a13714d575023858140396e0e05
Muskan Vaswan <muskanvaswan@gmail.com>
2021-04-03 12:14:56 +0530

    fix: making title div inheret from typography

--- client/src/components/index/Banner.js
+++ client/src/components/index/Banner.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import Typography from '@material-ui/core/Typography'
+
+
+
 
 export default function Banner() {
   return (
     <React.Fragment>
-      <div className="uppercase transition-colors duration-300 select-none text-blurple text-7xl md:text-8xl xl:text-9xl font-kiwi hover:text-indigo-600">
+      <Typography variant="div" className="uppercase transition-colors duration-300 select-none text-blurple text-7xl md:text-8xl xl:text-9xl font-kiwi hover:text-indigo-600">
         Salve
-      </div>
+      </Typography>
       <Link href={{ pathname: "survey", query: { question: 1 } }}>
         <div className="px-4 py-2 font-bold tracking-widest uppercase transition-colors duration-300 border-2 rounded-sm cursor-pointer sm:text-lg md:text-xl text-spotify-green hover:text-black hover:bg-spotify-green border-spotify-green hover:border-black">
           Take the initiation test
