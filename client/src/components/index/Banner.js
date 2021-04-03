3ee9b7c69f943dfa8313a32b2ae4de8791d7cd20
Muskan Vaswan <muskanvaswan@gmail.com>
2021-04-04 00:48:45 +0530

    feat(Banner): changing button text

--- client/src/components/index/Banner.js
+++ client/src/components/index/Banner.js
@@ -41,7 +41,7 @@ export default function Banner() {
       </Container>
       <Link href="/connect" underline="none">
         <div className="px-2 py-2 text-sm font-bold tracking-widest uppercase transition-colors duration-300 border-2 rounded-sm cursor-pointer md:px-4 sm:text-lg md:text-xl text-spotify-green hover:text-black hover:bg-spotify-green border-spotify-green hover:border-black">
-          Take the initiation test
+          Take the First Step towards Healing
         </div>
       </Link>
     </React.Fragment>
